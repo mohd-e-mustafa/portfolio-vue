197308ac466c49fdf0e448326f654b43578a80f0
mohd-e-mustafa <mohd.mustafa332@gmail.com>
2021-03-23 23:33:54 +0500

    Version 1.1.1
    Adding dist folder

---
+++ js/chunk-7461c9a8.f2acd80f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-7461c9a8"],{"1e9f":function(n,w,c){}}]);
+//# sourceMappingURL=chunk-7461c9a8.f2acd80f.js.map
---
+++ js/chunk-771a1063.56a26456.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-771a1063"],{d40f:function(n,w,o){}}]);
+//# sourceMappingURL=chunk-771a1063.56a26456.js.map
